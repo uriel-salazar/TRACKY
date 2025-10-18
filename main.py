f12989479f3db4c6d382c997251469cc2a19e5bc
uriel-salazar <urielix987@gmail.com>
2025-10-18 13:22:41 -0600

    menu created

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
 
-from texts import activity_level
-from control import TDEE
-from wrap_up import info_and_bmr,set_macros
-from open_food.wrap_api import calling
-
+from menu import menu_user
 def main():
-    value_bmr,wg,name=info_and_bmr()  #Calculates basic info and BMR
-    type_act,activity = activity_level() #Type of activity 
-    show_tdee=TDEE(activity,type_act,value_bmr) #Calculates your TDEE in base of your activity
-    set_macros(wg,name,show_tdee) #Shows the processed information in a tab format.
-    #and saves it in a .csv file
-    #calling api:
-    total_cal, total_prote, total_carb, total_fat = calling()
+    menu_user()
     ###
 if __name__ == "__main__":
     main()
--- menu.py
+++ menu.py
@@ -4,21 +4,30 @@ from control import TDEE
 from wrap_up import info_and_bmr,set_macros
 from open_food.wrap_api import calling
 
-
 def menu_user():
+    completed_info=False
     while True:
-        print("1.Calculate your BMR ")
-        print(" 2 .Calculate your TDEE and show your TDEE")
-        choice=int(input("Choice a number"))
+        print(" --- Macro Tracker --- ")
+        print("1.Calculate your BMR : ")
+        print(" 2 .Calculate your TDEE based on your activity")
+        print("3. Search an ingredient ")
+        choice=int(input("Choice a number (1/2/3):"))
         if choice==1:
             value_bmr,wg,name=info_and_bmr()
+            completed_info=True
+        
         elif choice==2:
-            type_act,activity = activity_level() #Type of activity 
-            show_tdee=TDEE(activity,type_act,value_bmr) #Calculates your TDEE in base of your activity
+            if completed_info:
+                type_act,activity = activity_level() #Type of activity 
+                show_tdee=TDEE(activity,type_act,value_bmr) #Calculates your TDEE in base of your activity
+                set_macros(wg,name,show_tdee)
+            else:
+                print("First,you must fill your basic information.")
         elif choice==3:
-            set_macros(wg,name,show_tdee)
+            calling()
         else:
-            print("Please enter a number ")
-                
+            print("Please,Enter a valid number")
+            
+            
             
         
