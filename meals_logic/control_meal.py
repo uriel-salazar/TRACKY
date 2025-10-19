1ea3bf14d6dee889300513ab28a60d8880e67732
uriel-salazar <urielix987@gmail.com>
2025-10-19 13:46:28 -0600

    .

--- meals_logic/control_meal.py
+++ meals_logic/control_meal.py
@@ -4,7 +4,27 @@ def track_meal():
     
     
 def manual_track():
-    pass
+    print("--Quick Add---")
+    name_food=input("Description :")
+    cal_food=input("Energy (cal) :")   
+    prote_food=input("Protein (g) :")
+    carb_food=input("Carb (g) : ")
+    fat_food=input("Enter your fat value ")
+    
+    user_info={
+        "Description":name_food,
+        "Calories":cal_food,
+        "Protein":prote_food,
+        "Carbs":carb_food,
+        "Fats":fat_food
+        
+    }
+    print("Save it as? :")
+    print("1.Breakfast, 2. Snack , 3.Meal , 4. Dinner")
+    enter_type=int(input("What are you going to choose??:"))
+    if enter_type==1:
+        name_track.append(breakfast_area)
+    
     
     
     
