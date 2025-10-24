375555d13507403f4ec46bddac9125effe742fa3
uriel-salazar <urielix987@gmail.com>
2025-10-24 13:17:21 -0600

    search product with api improved

--- open_food/wrap_api.py
+++ open_food/wrap_api.py
@@ -1,19 +1,23 @@
 from open_food.spoon import cal_api,search_product,portion,personalize_gr
-from open_food.show_ingr import product_macro
+from open_food.show_ingr import product_macro,csv_meal,food_added
 
 
 def calling():
-    
     url = cal_api()
+    name,calories, protein_100g, carbs_100g, fat_100g = search_product(url)
 
+    if calories is None:
+        print("Your food couldn't be calculated")
+        return None, None, None, None  # Stop here safely
 
-    calories, protein_100g, carbs_100g, fat_100g = search_product(url)
-
+    # If product found
     grams = portion()
-
     total_cal, total_prote, total_carb, total_fat = personalize_gr(
         calories, protein_100g, carbs_100g, fat_100g, grams
     )
-
-    product_macro(total_cal, total_prote, total_carb, total_fat)
+    m_cal,m_prote,m_carb,m_fat=product_macro(total_cal, total_prote, total_carb, total_fat)
+    file,meal_api=csv_meal(name,m_cal,m_prote,m_carb,m_fat)
+    food_added(file)
+    
     return total_cal, total_prote, total_carb, total_fat
+
--- see_macros.py
+++ see_macros.py
@@ -1,19 +1,24 @@
 
 import pandas as pd
+import numpy as np
 
 def check_macro():
     read_macro=pd.read_csv("data_macros.csv")
-    name_user=read_macro.iloc[-1]['Name']
-    protein_value=read_macro.iloc[-1]['Protein']
-    fat_value=read_macro.iloc[-1]['Fat']
-    cal_value=read_macro.iloc[-1]["Total Calories"]
-    print(name_user,protein_value,cal_value,fat_value)
+    name_user=str(read_macro.iloc[-1]['Name'])
+    protein_value=int(read_macro.iloc[-1]['Protein'])
+    fat_value=int(read_macro.iloc[-1]['Fat'])
+    cal_value=int(read_macro.iloc[-1]["Total Calories"])
     macro_user={
-        "User"
+        "User":name_user,
+        "Protein":protein_value,
+        "Fats":fat_value,
+        "Goal Calories":cal_value
+        
     }
+    return macro_user
 
 
-def check_data():
-    while True:
+
+
         
         
