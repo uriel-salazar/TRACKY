60f027896ed3d8b19fff9d38ca8daa137875a6d5
uriel-salazar <urielix987@gmail.com>
2025-10-27 19:53:23 -0600

    pulling values from the user data

--- open_food/actual_macros.py
+++ open_food/actual_macros.py
@@ -3,8 +3,17 @@ from saving_data import basic_data
 
 
 def remaining(group):
-    if group=="MALE":
-        user_data=pd.read_csv("bmr_man.csv")
+    if group == "MALE":
+        user_data = pd.read_csv("data_macros.csv")
     else:
-        user_data=pd.read_csv("bmr_woman.csv")
-    print(group)
+        user_data = pd.read_csv("data_macros.csv")
+
+    print("Your macros in gr:")
+    print(user_data.iloc[0].to_string(index=True))
+    name_main=user_data.loc[0,"Name"]
+    fat_main=user_data.loc[0,"Fat"]
+    prote_main=user_data.loc[0,"Protein"]
+    carb_main=user_data.loc[0,"Carbs"]
+    calories_main=user_data.loc[0,"Total Calories"]
+    
+    
