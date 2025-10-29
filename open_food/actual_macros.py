fbb633295ca1d1fb1cc42bf6f6f9c6336a0e4ea5
uriel-salazar <urielix987@gmail.com>
2025-10-28 19:46:38 -0600

    problem with returning macros

--- open_food/actual_macros.py
+++ open_food/actual_macros.py
@@ -1,12 +1,10 @@
 import pandas as pd
-from saving_data import basic_data
 
 
-def remaining(group):
-    if group == "MALE":
-        user_data = pd.read_csv("data_macros.csv")
-    else:
-        user_data = pd.read_csv("data_macros.csv")
+
+def remaining():
+    user_data = pd.read_csv("data_macros.csv")
+      
 
     print("Your macros in gr:")
     print(user_data.iloc[0].to_string(index=True))
@@ -15,5 +13,27 @@ def remaining(group):
     prote_main=user_data.loc[0,"Protein"]
     carb_main=user_data.loc[0,"Carbs"]
     calories_main=user_data.loc[0,"Total Calories"]
+    return name_main,fat_main,prote_main,carb_main,calories_main
+
+
+def goal_user(name, fat_goal, prote_goal, carb_goal, cal_goal, 
+               fat_u, prote_u, carb_u,cal_u):
+    fat_goal = int(fat_goal)
+    prote_goal = int(prote_goal)
+    carb_goal = int(carb_goal)
+    cal_goal = int(cal_goal)
+    fat_u = int(fat_u)
+    prote_u = int(prote_u)
+    carb_u = int(carb_u)
+    cal_u = int(cal_u)
+
+    print(f'Hii {name}!'  )
+    full_cal=cal_goal-cal_u
+    full_carb=carb_goal-carb_u
+    full_prote=prote_goal-prote_u
+    print(f"CAL:{full_cal}")
+    print(f" CARBS :  {full_carb}")
+    print(f"PROTE : {full_prote}")
+   
     
     
